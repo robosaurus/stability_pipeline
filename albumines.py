cf4e44f9d50f27d53d454a47bdb0cd9787d88aa7
robosaurus <lassesfantastiskeemail@gmail.com>
2018-11-22 12:56:27 +0100

    Added a method for getting exac and clinvar variants

--- albumines.py
+++ albumines.py
@@ -41,6 +41,59 @@ class albumin:
         ## the response_body a json object, let's parse it as a python dict.
         #self.isoforms_list = json.loads(r.text)
 
+    def get_variants(self, uniprotAC):
+        '''This method gets the variation data from uniprot and stores it under self.exac_variants and self.clinvar_variants.
+        For now it only records ClinVar and ExAC entries'''
+
+        # let's put the variants in these dictionaries.
+        self.exac_variants = {}
+        self.clinvar_variants = {}
+
+        # documentation of the proteins API: https://www.ebi.ac.uk/proteins/api/doc/#!/variation/getVariation
+
+        requestURL = "https://www.ebi.ac.uk/proteins/api/variation?offset=0&size=100&accession=" + uniprotAC
+        r = requests.get(requestURL, headers={"Accept": "application/json"})
+
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit()
+
+        responseBody = r.text
+
+        # the response is a json file, with the variation information
+        # read it as a python dict
+        variation_dictionary_list = json.loads(responseBody)
+        # they return a 1 length list for some reason
+        variation_dictionary = variation_dictionary_list[0]
+
+        # the actual variants are stored under variation_dictionary['features'], as a list, of dicts.
+        # with the following keys:
+        # dict_keys(['type', 'description', 'alternativeSequence', 'begin', 'end', 'xrefs', 'wildType', 'somaticStatus', 'cytogeneticBand', 'consequenceType', 'genomicLocation', 'association', 'clinicalSignificances', 'sourceType'])
+
+        for i in range(0, len(variation_dictionary['features'])-1):
+            type = variation_dictionary['features'][i]['consequenceType']
+            wtAA = variation_dictionary['features'][i]['wildType']
+            # alt_seq is the just the alt_aa in case of sAA subs.
+            alt_seq = variation_dictionary['features'][i]['alternativeSequence']
+            # the 'begin' and 'end' value is always the same. So let's go with 'begin'.
+            pos = variation_dictionary['features'][i]['begin']
+
+            # for now we only care about exac and clinvar variants
+            # they will have a key called 'xrefs'
+            if 'xrefs' in variation_dictionary['features'][i]:
+                # one variant can have several xrefs
+                for element in variation_dictionary['features'][i]['xrefs']:
+                    # check if the variant is referenced in ExAC or ClinVar
+                    # and put them in the dicts.
+                    # I would have like to have the substiturion as a key, and the ids as values.
+                    # but since several variants can have the same sub, we have to do it the other way around.
+                    if element['name'] == 'ClinVar':
+                        self.clinvar_variants[element['id']] = [wtAA + pos + alt_seq, type]
+                        # TO DO: add clinical significance, maybe
+                    elif element['name'] == 'ExAC':
+                        self.exac_variants[element['id']] = [wtAA + pos + alt_seq, type]
+                        # To DO: add frequuencies
+                        # this will most likely be a seperate lookup with the eXac API
 
     def get_swismodel(self):
         # same as pdb, but for homology models.
