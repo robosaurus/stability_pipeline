f54d4dada6ece4208c60460fe7e3ff0c3bb4553a
robosaurus <lassesfantastiskeemail@gmail.com>
2018-11-27 13:00:45 +0100

    added a method for mapping pdb residues to uniprot numbering

--- structure_class.py
+++ structure_class.py
@@ -4,6 +4,7 @@ import json
 import sys
 import subprocess
 import os
+import xml.etree.ElementTree as ET
 # and some functions for parsing ddg_predictions
 from parse_cartesian_functions import rosetta_cartesian_read, ddgs_from_dg
 
@@ -48,6 +49,7 @@ class structure:
         # PDBe has it ordered so that the 'best' is first
         # each element in the structure list is a dictionary.
         self.best_structure = structure_list[0]['pdb_id']
+        self.pdb_id = structure_list[0]['pdb_id']
         self.chain_id = structure_list[0]['chain_id']
         # coverage seems like a key parameter, let's record that too
         self.coverage = structure_list[0]['coverage']
@@ -192,13 +194,6 @@ class structure:
                 mutfile.write(self.fasta_seq[residue_number-1] + ' ' + str(residue_number) + ' ' + AAtype + '\n')
             mutfile.close()
 
-    # now we are about ready to submit some rosetta jobs
-    # i am not sure how to go about that.
-    # slurm maybe?
-    # or should it be run locally?
-    # let us start with a local boy
-    # there is the relaxation of the structure first.
-
     def rosetta_relax(self):
         '''this function sruns a rosetta relaxation of the structure,
         it follows the recomended protocol for a pre-relaxation prior to
@@ -255,7 +250,7 @@ echo $INDEX
     '''.format(len(self.fasta_seq), self.path_to_rosetta, self.path_to_rosetta[0:-7]+'/database/', '*_bn15_calibrated*.pdb'))
         sbatch.close()
 
-    def parse_rosetta_ddgs(self, exac_variants='', clinvar_variants=''):
+    def parse_rosetta_ddgs(self, exac_variants='', clinvar_variants='', pdb_to_uniprot = self.pdb_to_uniprot):
         '''This function parses the result of a Rosetta ddg submission,
         It returns a dictionary with the variants as keys, and the ddgs as values.
         It only works if the sbatch job has finished.'''
@@ -282,7 +277,7 @@ echo $INDEX
         scorefile_line = '{}' + '\t {:.3}'*20 + '\n'
         for i in range(1, len(self.fasta_seq.strip()) + 1):
             try:
-                scorefile.write(scorefile_line.format(str(i), self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'A'],  self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'C'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'D'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'E'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'F'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'G'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'H'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'I'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'K'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'L'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'M'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'N'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'P'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'Q'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'R'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'S'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'T'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'V'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'W'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'Y']))
+                scorefile.write(scorefile_line.format(pdb_to_uniprot[i], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'A'],  self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'C'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'D'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'E'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'F'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'G'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'H'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'I'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'K'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'L'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'M'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'N'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'P'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'Q'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'R'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'S'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'T'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'V'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'W'], self.rosetta_cartesian_ddgs_dict[self.fasta_seq[i-1]+str(i)+'Y']))
             except(KeyError):
                 print('missing DATA! ', i)
 
@@ -310,9 +305,51 @@ echo $INDEX
     def align_pdb_to_uniprot(self):
         '''This method gets the alignment of the pdb residues to the uniprot sequence.
         The alignment is fetched from SIFTS.
-        It is stored under self.alignment'''
+        A dictionary is stored under self.pdb_to_uniprot, that has the chain IDs as keys and a dictionary as value.
+        This dictionary has the pdb numbers as keys, and the uniprot numbering as value.'''
 
         # in order to maintain the uniprot numbering scheme we need an alignment of the pdb residues
         # to the uniprot sequence. We get it from SIFTS, the same system that helped us find pdbs in
         # the first place.
+        #        http://www.ebi.ac.uk/pdbe/api/mappings/uniprot_segments/:accession
+        # sifts also has an xml based api. That has some of the same information.
+        # but they seem in the process of phasing that out. So let's go with the new one.
 
+        requestURL = 'http://www.ebi.ac.uk/pdbe/api/mappings/uniprot_segments/{}'.format(self.pdb_id)
+        r = requests.get(requestURL)
+        # we check that this is an entry at in the database
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit()
+        mapping_dict = json.loads(r.text)
+        # the uniprot mappings will be here:
+        # mapping_dict[self.pdb_id]['UniProt']
+        # it is a dictionary with each uniprot ID present in the structure, as a key
+        # let's put he ones we are currently working on here:
+        uniprot_segments = mapping_dict[self.pdb_id]['UniProt'][self.uniprotac]['mappings']
+        # this is a list of segments from the structure that are present in the uniprot.
+        # each element is a dictionary with the following keys:
+        # dict_keys(['entity_id', 'end', 'chain_id', 'start', 'unp_end', 'unp_start', 'struct_asym_id'])
+        # lets make a dictionary, with the pdb numbering as keys, and the uniprot numbering as values
+        # because of chains, and the possibility of the same number in different chains,
+        # it will be a dictionary of chain dictionaries.
+        self.pdb_to_uniprot = {}
+        for mapping in uniprot_segments:
+            print('new mapping')
+            # make a chain dict in the pdb_to_uniprot dict:
+            self.pdb_to_uniprot[mapping['chain_id']] = {}
+            print(mapping)
+            # for residue number in pdb mapping:
+            for residue in range(int(mapping['start']['residue_number']), int(mapping['end']['residue_number'])+1):
+                # for this residue the unprot numbering is:
+                uniprot_number = int(mapping['unp_start']) + residue - 1
+                # assign the number in the chain dictionary to the corresponding uniprot ID
+                self.pdb_to_uniprot[mapping['chain_id']][residue] = uniprot_number
+
+        print(self.pdb_to_uniprot)
+
+        def gently_clean_pdb():
+            # here is a method for cleaning the pdb, that is less drastic than clean and isolate.
+            # and we need to keep track of the numbering.
+
+            pass
