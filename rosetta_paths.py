333deec908bb12dfc34ff41d0459971f0a43af5b
robosaurus <lassesfantastiskeemail@gmail.com>
2018-12-06 17:03:13 +0100

    a module with rosetta paths

---
+++ rosetta_paths.py
@@ -0,0 +1,6 @@
+# here are some paths for rosetta and some utility scripts
+# remember to update if rosetta moves
+path_to_parameters = "/Users/kuwebatake/stability_pipeline/rosetta_parameters"
+path_to_rosetta = "/groups/sbinlab/software/Rosetta_2018_Oct_d557f8/"
+path_to_clean_pdb = "/Users/kuwebatake/stability_pipeline/clean_pdb.py"
+path_to_clean_keep_ligand = "/Users/kuwebatake/stability_pipeline/clean_pdb.py"
