8ec9fb117ce66d54c067f3041e039967d00abf68
robosaurus <lassesfantastiskeemail@gmail.com>
2018-12-12 17:35:19 +0100

    changed the uniprot acs

--- run_test_set.py
+++ run_test_set.py
@@ -2,5 +2,5 @@ from predict_stability import predict_stability_for_ac
 
 output_folder = '/groups/sbinlab/software/stability_pipeline/output/'
 
-for uniprot_accession in ['P43246', 'P40692', 'P35557', 'P51587', 'P60484', 'Q8NEA6']:
+for uniprot_accession in ['O00631']:
         predict_stability_for_ac(uniprot_accession, output_folder)
